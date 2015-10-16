972dbc5d6861602b03ff6dc11228a5a17bcfab2b
Nitin Shukla <nitincontactme@gmail.com>
2015-10-16 16:41:03 +0530

    Created by Coding Ground Client

---
+++ New Project/Newfile(1).vb
@@ -0,0 +1,12 @@
+Module BFM
+'-------------------------------------------------------------
+' Sub :  I2C_Read
+'------------------------------------------------------------
+Public Sub I2C_Read(DevAddr As  Long , RegAddr As  Long , ReadData As  Long)
+End Sub
+'-------------------------------------------------------------
+' Sub :  I2C_Write
+'------------------------------------------------------------
+Public Sub I2C_Write(DevAddr As  Long , RegAddr As  Long , Data As  Long)
+End Sub
+End Module
